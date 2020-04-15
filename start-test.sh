a35ee7f74d8ce076c08dde92a8a3304c33cbaf86
Daniel Stoinov <daniel.stoinov@gmail.com>
2020-04-15 00:00:52 +0000

    added the scripts to the start-test

--- start-test.sh
+++ start-test.sh
@@ -2,11 +2,11 @@
 
 dname=$(dirname $0)
 
-bash $dname/1.sh && \
-bash $dname/2.sh && \
-bash $dname/3.sh && \
-bash $dname/4.sh && \
+bash $dname/nasa.sh && \
+bash $dname/nasa2.sh && \
+#bash $dname/3.sh && \
+#bash $dname/4.sh && \
 #bash 4-fail.sh && \
-bash $dname/multistep1/multistep1.sh && \
+#bash $dname/multistep1/multistep1.sh && \
 exit 0 || exit 1
 
